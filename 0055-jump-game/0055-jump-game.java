85a8351caf283e330ffbf238d24ff417c72d820b
KUSHEEN Dhar <kusheendhar942@gmail.com>
2026-01-31 23:17:03 +0530

    Time: 2 ms (85.42%), Space: 47.9 MB (55.16%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,11 +1,11 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int fartest = 0;
-        for(int i=0;i<nums.length;i++){
-            if(i>fartest){
+        int first = 0;
+        for(int i = 0;i<nums.length;i++){
+            if(i>first){
                 return false;
             }
-            fartest = Math.max(fartest,i+nums[i]);
+            first=Math.max(first,i+nums[i]);
         }
         return true;
     }
