fc41f65db24bf20b0c8b7f15fc238dd6ee46d38d
KUSHEEN Dhar <kusheendhar942@gmail.com>
2026-04-16 14:01:30 +0530

    Time: 199 ms (1.87%), Space: 77.2 MB (53.61%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -3,11 +3,12 @@ class Solution {
         int sum = 0;
         int maxi = nums[0];
         for(int i =0;i<nums.length;i++){
-            sum = sum + nums[i];
-            maxi = Math.max(maxi,sum);
-        if(sum <0){
-            sum = 0;
-        }
+            sum += nums[i];
+            maxi=Math.max(sum,maxi);
+
+            if(sum<0){
+                sum =0;
+            }
         }
         return maxi;
     }
