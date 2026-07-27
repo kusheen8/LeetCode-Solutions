b9f844a04ed934820f8538ef8917c4def1e6231b
KUSHEEN Dhar <kusheendhar942@gmail.com>
2026-07-27 18:24:17 +0530

    Time: 1 ms (61.96%), Space: 43.4 MB (17.08%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -3,9 +3,9 @@ class Solution {
         StringBuilder ans = new StringBuilder();
         Arrays.sort(strs);
         String first = strs[0];
-        String last = strs[strs.length -1];
-        for(int i =0;i<Math.min(first.length(), last.length());i++){
-            if(first.charAt(i)!= last.charAt(i)){
+        String last = strs[strs.length-1];
+        for(int i=0;i<Math.min(first.length(),last.length());i++){
+            if(first.charAt(i)!=last.charAt(i)){
                 return ans.toString();
             }
             ans.append(first.charAt(i));
