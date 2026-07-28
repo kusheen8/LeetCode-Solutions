e499d9fd5885bd48184141c0e353487d96f45d0b
KUSHEEN Dhar <kusheendhar942@gmail.com>
2026-07-28 16:40:08 +0530

    Time: 2 ms (28.21%), Space: 45.4 MB (49.21%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,22 +1,22 @@
 class Solution {
     public int compress(char[] chars) {
-        StringBuilder sb = new StringBuilder();
         int i = 0;
+        StringBuilder sb = new StringBuilder();
         while(i<chars.length){
             int j = i;
-            while(j<chars.length && chars[i] == chars[j]){
+            while(j<chars.length && chars[i]==chars[j]){
                 j++;
             }
             sb.append(chars[i]);
             int len = j-i;
-            if(len > 1){
+            if(len >1){
                 sb.append(len);
             }
             i=j;
         }
-        for(i=0;i<sb.length();i++){
-            chars[i]=sb.charAt(i);
-        }
+            for(i=0;i<sb.length();i++){
+                chars[i] = sb.charAt(i);
+            }
         return sb.length();
     }
 }
