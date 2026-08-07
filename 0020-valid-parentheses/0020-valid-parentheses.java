1ef4d910298a4a96a0ae42b3b103188b672a9113
KUSHEEN Dhar <kusheendhar942@gmail.com>
2026-08-08 01:23:47 +0530

    Time: 3 ms (86.01%), Space: 43.1 MB (86.23%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -2,23 +2,23 @@ class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
         for(char ch : s.toCharArray()){
-            if(ch == '(' || ch == '[' || ch == '{'){
+            if(ch == '(' || ch == '{' || ch == '['){
                 stack.push(ch);
             }else{
                 if(stack.isEmpty()){
                     return false;
                 }
-                int top = stack .pop();
-                if(ch == ')' && top!='('){
-                    return false;
-                }
-                if(ch == ']' && top!= '['){
-                    return false;
-                }
-                if(ch == '}' && top != '{'){
-                    return false;
-                }
+            int top = stack.pop();
+            if(ch == ')' && top != '('){
+                return false;
             }
+            if(ch == ']' && top!= '['){
+                return false;
+            }
+            if(ch == '}' && top!= '{'){
+                return false;
+            }
+        }
         }
         return stack.isEmpty();
     }
